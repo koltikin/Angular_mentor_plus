00b95edda0f8ab8f940ac7f3cde4fd899da617e5
owermaiden <ower.maiden@cybertekschool.com>
2023-05-02 10:28:48 -0400

    string interpolation

--- src/app/user/user-create/user-create.component.ts
+++ src/app/user/user-create/user-create.component.ts
@@ -8,4 +8,14 @@ import { User } from 'src/app/shared/user';
 })
 export class UserCreateComponent {
   userList : User[] = [];
+
+  addUser(item : any){
+    let user : User = {username: item.username , password : item.password };
+    this.userList.push(user);
+  }
+
+  deleteUser(name : string){
+    let index : number = this.userList.findIndex(d => d.username === name);
+    this.userList.splice(index, 1);
+  }
 }
